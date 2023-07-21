d055c62c03cd1b7be2f41025c65534d64fc98b56
bubachita <bubachiita@gmail.com>
2023-07-21 22:05:33 +0600

    fix some problems

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,6 +8,6 @@ program
   .version('1.0.0')
   .arguments('<filepath1> <filepath2>')
   .option('-f, --format <type>', 'output format')
-  .action((filepath1, filepath2) => genDiff(filepath1, filepath2));
+  .action((filepath1, filepath2) => console.log(genDiff(filepath1, filepath2)));
 
 program.parse();
