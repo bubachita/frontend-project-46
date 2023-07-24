5839cb5700200ea50b0753e37034ee79573e860d
bubachita <bubachiita@gmail.com>
2023-07-24 14:10:16 +0600

    add test for yaml

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,4 +1,3 @@
-import { test, expect } from '@jest/globals';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 import fs from 'fs';
@@ -8,10 +7,13 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const getFixturePath = (filename) => join(__dirname, '..', '__fixtures__', filename);
 
-const expectedResult = fs.readFileSync(getFixturePath('result.txt'), 'utf8');
-const file1 = getFixturePath('file1.json');
-const file2 = getFixturePath('file2.json');
-
-test('gendiff', () => {
-  expect(genDiff(file1, file2)).toEqual(expectedResult);
+test.each([
+  ['json', 'json'],
+  ['yaml', 'yaml'],
+  ['yml', 'yml'],
+])('gendiff', (extension1, extension2) => {
+  const file1 = getFixturePath(`file1.${extension1}`);
+  const file2 = getFixturePath(`file2.${extension2}`);
+  const result = fs.readFileSync(getFixturePath(`result.${extension1}.txt`), 'utf8');
+  expect(genDiff(file1, file2)).toEqual(result);
 });
